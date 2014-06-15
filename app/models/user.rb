862abec34b761e8bda61a65510dfd85d33f325ea
Andrew Tumelty <andrew.tumelty@gmail.com>
2014-06-15 21:25:22 +0100

    Login, logout, register views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  validates :email, uniqueness: true
+
+  before_create :set_auth_token
+
+  private
+    def set_auth_token
+      return if auth_token.present?
+
+      begin
+        self.auth_token = SecureRandom.hex
+      end while self.class.exists?(auth_token: self.auth_token)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+
   root :to => redirect('/movies')
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
+  resources :users
+
+  # get 'user_sessions/new'
+  # get 'user_sessions/create'
+  # get 'user_sessions/destroy'
+
   get 'movies' => 'movie#index', :as => :movies
   get 'jobs' => 'job#index'
   get 'job/:action' => 'job'
