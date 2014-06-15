93e9e51e11e6e83a4be374b7dadae457dcdcc7ba
Andrew Tumelty <andrew.tumelty@gmail.com>
2014-06-15 20:18:35 +0100

    Sorcery setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140517131712) do
+ActiveRecord::Schema.define(version: 20140615191712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,26 @@ ActiveRecord::Schema.define(version: 20140517131712) do
   end
 
   add_index "movies_writers", ["writer_id", "movie_id"], name: "index_movies_writers_on_writer_id_and_movie_id", unique: true, using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email",                           null: false
+    t.string   "crypted_password",                null: false
+    t.string   "salt",                            null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "last_login_at"
+    t.datetime "last_logout_at"
+    t.datetime "last_activity_at"
+    t.string   "last_login_from_ip_address"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.string   "auth_token",                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
 
   create_table "writers", force: true do |t|
     t.string   "writer"
